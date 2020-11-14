5fad68f71f6d220e83ade67b7e4f73de9e7d9250
Javier Laguna <javier.laguna92@gmail.com>
2020-11-14 11:48:35 +0100

    Add basic LoginViewController

---
+++ EhHoFinal/Scenes/Login/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  EhHoFinal
+//
+//  Created by Javier Laguna on 14/11/2020.
+//  Copyright © 2020 Roberto Garrido. All rights reserved.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet weak private var usernameTextField: UITextField!
+    @IBOutlet weak private var passwordTextField: UITextField!
+    @IBOutlet weak private var loginButton: UIButton!
+    
+    // MARK: Properties
+    
+    
+    // MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
